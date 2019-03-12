cc9b0754353223889a0f14bd975946e6b93270f5
Marcin Wielgus <mwielgus@outlook.com>
2019-03-12 02:02:58 +0100

    error in script

--- nexus/stack/scripts/security.groovy
+++ nexus/stack/scripts/security.groovy
@@ -17,18 +17,18 @@ def repoMvnGroupName = "${name}-mvn-group"
 def allRepoNames = [repoDockerHostedName,
                  repoDockerReleasesName,
                  repoDockerProxyName,
-                 repoDockerGroupName].collect { "docker-$it" } +
+                 repoDockerGroupName].collect { "docker-$it".toString() } +
         [repoMvnHostedName,
          repoMvnReleasesName,
          repoMvnProxy1Name,
          repoMvnProxy2Name,
-         repoMvnGroupName].collect { "maven2-$it" }
+         repoMvnGroupName].collect { "maven2-$it".toString() }
 
-def snapshotRepoNames = [repoDockerHostedName].collect { "docker-$it" } +
-        [repoMvnHostedName].collect { "maven2-$it" }
+def snapshotRepoNames = [repoDockerHostedName].collect { "docker-$it".toString() } +
+        [repoMvnHostedName].collect { "maven2-$it".toString() }
 
-def releaseRepoNames = [repoDockerHostedName].collect { "docker-$it" } +
-        [repoMvnHostedName].collect { "maven2-$it" }
+def releaseRepoNames = [repoDockerHostedName].collect { "docker-$it".toString() } +
+        [repoMvnHostedName].collect { "maven2-$it".toString() }
 
 
 def usersExist = false
@@ -44,8 +44,8 @@ log.info('Anonymous access disabled')
 if (!usersExist) {/*To create new admin user you will have to assign nx-admin role*/
 /*Create a new role that allows a user same access as anonymous and adds healtchcheck access*/
     def devPrivileges = ['nx-healthcheck-read', 'nx-healthcheck-summary-read', 'nx-search-read'] +
-                allRepoNames.collect{"nx-repository-view-$it-read"} +
-                allRepoNames.collect{"nx-repository-view-$it-browse"}
+                allRepoNames.collect{"nx-repository-view-$it-read".toString()} +
+                allRepoNames.collect{"nx-repository-view-$it-browse".toString()}
     def anoRole = []
 /* add roles that uses the built in nx-anonymous role as a basis and adds more privileges*/
     security.addRole('developer', 'Developer', 'User with privileges to allow read access to repo content and healtcheck', devPrivileges, anoRole)
@@ -56,8 +56,8 @@ if (!usersExist) {/*To create new admin user you will have to assign nx-admin ro
     log.info('User developer created')
 /*Create new role that allows deployment and create a user to be used on a CI server*/
 /* privileges with pattern * to allow any format, browse and read are already part of nx-anonymous*/
-    def depPrivileges = snapshotRepoNames.collect{"nx-repository-view-$it-add"} +
-            snapshotRepoNames.collect{"nx-repository-view-$it-edit"}
+    def depPrivileges = snapshotRepoNames.collect{"nx-repository-view-$it-add".toString()} +
+            snapshotRepoNames.collect{"nx-repository-view-$it-edit".toString()}
     def roles = ['developer']
 /* add roles that uses the developer role as a basis and adds more privileges*/
     security.addRole('deployer', 'Deployer', 'User with privileges to allow deployment snap repositories', depPrivileges, roles)
@@ -66,8 +66,8 @@ if (!usersExist) {/*To create new admin user you will have to assign nx-admin ro
     def lJenkins = security.addUser('ci', 'ci', 'automatic', '${SHATHEL_ENV_NEXUS_CI_MAIL}', true, '${SHATHEL_ENV_NEXUS_CI_PASS}', depRoles)
 
 
-    def relPrivileges = releaseRepoNames.collect{"nx-repository-view-$it-add"} +
-            releaseRepoNames.collect{"nx-repository-view-$it-edit"}
+    def relPrivileges = releaseRepoNames.collect{"nx-repository-view-$it-add".toString()} +
+            releaseRepoNames.collect{"nx-repository-view-$it-edit".toString()}
     def relRoles = ['developer', 'deployer']
     security.addRole('releaser', 'Releaser', 'User with privileges to allow deployment release repositories', relPrivileges, relRoles)
     security.addUser('release', 'release', 'automatic', '${SHATHEL_ENV_NEXUS_RELEASE_MAIL}', true, '${SHATHEL_ENV_NEXUS_RELEASE_PASS}', ['releaser'])
